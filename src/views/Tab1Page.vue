0282be0742d8d7eccaf7bdac5929be3d142c2bc1
dangtin306 <dangtin306@gmail.com>
2022-09-16 12:05:59 +0700

    okkkk

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,16 +8,16 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Tab 111 page" />
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
